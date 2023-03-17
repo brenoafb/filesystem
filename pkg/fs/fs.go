a3f19fba22083f09d22e78da5ae9c8ae5751ec9e
brenoafb <breno.afb@gmail.com>
2023-03-16 23:26:22 -0300

    Fix tests, abstract elementary operations

--- pkg/fs/fs.go
+++ pkg/fs/fs.go
@@ -32,31 +32,45 @@ const (
 	InodeSize = 512  // bytes
 )
 
-type InodeType int
+type InodeType uint32
 
 const (
 	// InodeTypeFile is a regular file.
-	InodeTypeFile = iota
+	InodeTypeFile InodeType = iota
 	// InodeTypeDirectory is a directory.
 	InodeTypeDirectory
 )
 
 type Inode struct {
-	Size     uint32 // in bytes
-	Index    uint32 // index in the inode list and bitmap
-	Type     InodeType
-	Blocks   [16]uint32 // block numbers
-	Filename string     // up to 128 bytes
+	// Size represents the size of the file in number of bytes
+	Size uint32
+	// Index represents the index of the inode
+	Index uint32
+	// Type indicates whether it's a regular file or a directory
+	Type InodeType
+	// Blocks contains the index of the blocks occupied by the file.
+	// If the file is smaller than 16 blocks, the remaining block indices
+	// are set to 0.
+	// Meaning that the blocks occupied by the file are B[0] through B[i],
+	// where i is the largest number for which B[i] > 0.
+	Blocks [16]uint32 // block numbers
+	// Filename contains the file's relative name.
+	// It can be up to 128 bytes in size.
+	Filename string
 	// ...
 }
 
 type FileSystem struct {
-	dev    BlockDevice
+	// dev is the underlying block device
+	dev BlockDevice
+	// inode list
 	inodes [32]*Inode
 	// For simplicity, we'll just use a byte array to represent the bitmaps.
 	// Each byte is either 0 or 1
+	// indicates which inodes are taken
 	inodeBitmap [32]byte // up to 32 inodes
-	dataBitmap  [32]byte // up to 32 blocks
+	// indicates which data blocks are taken
+	dataBitmap [32]byte // up to 32 blocks
 }
 
 func NewFileSystem(dev BlockDevice) (*FileSystem, error) {
@@ -76,15 +90,15 @@ func NewFileSystem(dev BlockDevice) (*FileSystem, error) {
 	if err != nil {
 		return nil, fmt.Errorf("error writing superblock: %w", err)
 	}
-	// write the inode bitmap (which is a 1)
+	// write the inode bitmap (which is a 1 since we have only the root dir inode)
 	buf = []byte{1}
-	err = dev.WriteBlock(1, buf)
+	err = dev.WriteBlock(InodeBitmapIndex, buf)
 	if err != nil {
 		return nil, fmt.Errorf("error writing inode bitmap: %w", err)
 	}
-	// write the data bitmap (which is a 1)
-	buf = []byte{1}
-	dev.WriteBlock(2, buf)
+	// write the data bitmap (which is a 0 since no data is allocated yet)
+	buf = []byte{0}
+	dev.WriteBlock(DataBitmapIndex, buf)
 
 	rootInode := &Inode{
 		Size:     0,
@@ -102,7 +116,7 @@ func NewFileSystem(dev BlockDevice) (*FileSystem, error) {
 		return nil, fmt.Errorf("error encoding root inode: %w", err)
 	}
 	buf = bb.Bytes()
-	dev.WriteBlock(3, buf)
+	dev.WriteBlock(InodeStartIndex, buf)
 
 	return &FileSystem{
 		dev:         dev,
@@ -158,7 +172,7 @@ func (fs *FileSystem) DisplayInfo() {
 			fmt.Printf("-- directory inode %d --\n", inodeIndex)
 		}
 
-		contents, err := fs.ReadInodeContents(uint32(inodeIndex))
+		contents, err := fs.ReadInodeContents(inodeIndex)
 
 		fmt.Printf("size: %d\n", inode.Size)
 		fmt.Printf("blocks: %v\n", inode.Blocks)
@@ -238,14 +252,14 @@ func LoadFilesystem(dev BlockDevice) (*FileSystem, error) {
 	}, nil
 }
 
-func (fs *FileSystem) GetInode(inodeIndex uint32) (*Inode, error) {
-    if inodeIndex >= 32 { // TODO remove hardcoded size
-        return nil, fmt.Errorf("inode index out of bounds: %d", inodeIndex)
-    }
-    return fs.inodes[inodeIndex], nil
+func (fs *FileSystem) GetInode(inodeIndex int) (*Inode, error) {
+	if inodeIndex >= 32 { // TODO remove hardcoded size
+		return nil, fmt.Errorf("inode index out of bounds: %d", inodeIndex)
+	}
+	return fs.inodes[inodeIndex], nil
 }
 
-func (fs *FileSystem) ReadInodeContents(inodeIndex uint32) (*bytes.Buffer, error) {
+func (fs *FileSystem) ReadInodeContents(inodeIndex int) (*bytes.Buffer, error) {
 	inode := fs.inodes[inodeIndex]
 
 	// read the blocks
@@ -259,10 +273,13 @@ func (fs *FileSystem) ReadInodeContents(inodeIndex uint32) (*bytes.Buffer, error
 		bb.Write(buf)
 	}
 
+	// trim to the correct file size
+	bb.Truncate(int(inode.Size))
+
 	return bb, nil
 }
 
-func (fs *FileSystem) ReadFileContents(inodeIndex uint32) (*bytes.Buffer, error) {
+func (fs *FileSystem) ReadFileContents(inodeIndex int) (*bytes.Buffer, error) {
 	inode := fs.inodes[inodeIndex]
 	if inode.Type != InodeTypeFile {
 		return nil, fmt.Errorf("inode %d is not a file", inodeIndex)
@@ -271,7 +288,7 @@ func (fs *FileSystem) ReadFileContents(inodeIndex uint32) (*bytes.Buffer, error)
 	return fs.ReadInodeContents(inodeIndex)
 }
 
-func (fs *FileSystem) ReadDir(inodeIndex uint32) ([]*Inode, error) {
+func (fs *FileSystem) ReadDir(inodeIndex int) ([]*Inode, error) {
 	// The directory is a list of node indices along with their filenames.
 	// Example
 	// 1 foo
@@ -304,8 +321,9 @@ func (fs *FileSystem) ReadDir(inodeIndex uint32) ([]*Inode, error) {
 	return inodes, nil
 }
 
-func (fs *FileSystem) AddFileToDir(dirInodeIndex uint32, fileInodeIndex uint32) error {
+func (fs *FileSystem) AddFileToDir(dirInodeIndex int, fileInodeIndex int) error {
 	// read the directory contents
+	inode := fs.inodes[dirInodeIndex]
 	contents, err := fs.ReadInodeContents(dirInodeIndex)
 	if err != nil {
 		return err
@@ -315,6 +333,46 @@ func (fs *FileSystem) AddFileToDir(dirInodeIndex uint32, fileInodeIndex uint32) 
 	contents.WriteString(fmt.Sprintf("%d %s\n", fileInodeIndex, fs.inodes[fileInodeIndex].Filename))
 	// update the size
 	fs.inodes[dirInodeIndex].Size = uint32(contents.Len())
+	// check if the current number of blocks allocated to the file suffice
+	nCurrentBlocks := 0
+	blockEndIndex := 0
+	for i, blockIndex := range inode.Blocks {
+		// Only nonzero blocks indicate actual blocks used by the file
+		// Whenever we reach a 0, it means that there are no more blocks taken
+		// by the file
+		if blockIndex == 0 {
+			blockEndIndex = i
+			break
+		}
+		nCurrentBlocks += 1
+	}
+
+	nTotalBlocks := GetSizeInBlocks(contents.Len())
+
+	if nTotalBlocks <= nCurrentBlocks {
+		// Current block count is enough
+	} else {
+		// We need extra blocks to fit the new content
+		// find nBlocks empty data blocks
+		added := 0
+		for i := 0; i < 32; i++ {
+			if fs.dataBitmap[i] == 0 {
+				// Found an empty block
+				// Remember that block indices are absolute,
+				// meaning that we have to add the start offset
+				inode.Blocks[blockEndIndex+added] = uint32(i) + DataStartIndex
+				fs.dataBitmap[i] = 1
+				added++
+				if added == nTotalBlocks-nCurrentBlocks {
+					break
+				}
+			}
+		}
+
+		if added < nTotalBlocks-nCurrentBlocks {
+			return fmt.Errorf("not enough free blocks to fit the new directory contents")
+		}
+	}
 
 	// write the new contents
 	err = fs.WriteInodeContents(dirInodeIndex, contents)
@@ -325,10 +383,13 @@ func (fs *FileSystem) AddFileToDir(dirInodeIndex uint32, fileInodeIndex uint32) 
 	// flush the inode table
 	err = fs.WriteInodeTable()
 
+	// write the data bitmap
+	fs.PersistDataBitmap()
+
 	return nil
 }
 
-func (fs *FileSystem) WriteInodeContents(inodeIndex uint32, contents *bytes.Buffer) error {
+func (fs *FileSystem) WriteInodeContents(inodeIndex int, contents *bytes.Buffer) error {
 	nBlocks := (contents.Len() + BlockSize - 1) / BlockSize
 	inode := fs.inodes[inodeIndex]
 	// write the data blocks
@@ -373,32 +434,11 @@ func (fs *FileSystem) WriteInodeTable() error {
 	return nil
 }
 
-func (fs *FileSystem) CreateFile(filename string, contents bytes.Buffer) (*Inode, error) {
-	path := strings.Split(filename, "/")
-	if path[0] != "" {
-		return nil, fmt.Errorf("filename must be absolute")
-	}
-	// find the parent directory
-	// start at the root inode
-	var parentInodeIndex uint32 = 0
-	parentInode := fs.inodes[parentInodeIndex]
-	for i := 1; i < len(path)-1; i++ {
-		children, err := fs.ReadDir(parentInodeIndex)
-		if err != nil {
-			return nil, fmt.Errorf("error reading directory %s: %w", path[i], err)
-		}
-		found := false
-		for _, child := range children {
-			if child.Filename == path[i] {
-				parentInodeIndex = child.Index
-				parentInode = child
-				found = true
-				break
-			}
-		}
-		if !found {
-			return nil, fmt.Errorf("directory %s not found", path[i])
-		}
+func (fs *FileSystem) CreateFile(filename string, contents *bytes.Buffer) (*Inode, error) {
+	parentInode, err := fs.FindParentInodeByName(filename)
+
+	if err != nil {
+		return nil, fmt.Errorf("error when finding parent inode: %w", err)
 	}
 
 	// check if the parent inode is a directory
@@ -406,35 +446,19 @@ func (fs *FileSystem) CreateFile(filename string, contents bytes.Buffer) (*Inode
 		return nil, fmt.Errorf("parent inode is not a directory")
 	}
 
-	// find an empty inode
-	inodeIndex := -1
-	for i := 0; i < 32; i++ {
-		if fs.inodeBitmap[i] == 0 {
-			inodeIndex = i
-			break
-		}
+	// find an free inode
+	inodeIndex, err := fs.FindFreeInode()
+
+	if err != nil {
+		return nil, fmt.Errorf("error when finding free inode: %w", err)
 	}
 
-	if inodeIndex == -1 {
-		return nil, fmt.Errorf("no empty inodes")
-	}
+	nBlocks := GetSizeInBlocks(contents.Len())
 
-	nBlocks := (contents.Len() + BlockSize - 1) / BlockSize
+	dataBlockIndices, err := fs.FindEmptyBlocks(nBlocks)
 
-	dataBlockIndices := []uint32{}
-
-	// find nBlocks empty data blocks
-	for i := 0; i < 32; i++ {
-		if fs.dataBitmap[i] == 0 {
-			dataBlockIndices = append(dataBlockIndices, uint32(i)+DataStartIndex)
-			if len(dataBlockIndices) == nBlocks {
-				break
-			}
-		}
-	}
-
-	if len(dataBlockIndices) != nBlocks {
-		return nil, fmt.Errorf("not enough empty data blocks")
+	if err != nil {
+		return nil, fmt.Errorf("error when finding blocks for new file: %w", err)
 	}
 
 	dataBlockIndicesArray := [16]uint32{}
@@ -446,41 +470,141 @@ func (fs *FileSystem) CreateFile(filename string, contents bytes.Buffer) (*Inode
 		Type:     InodeTypeFile,
 		Size:     uint32(contents.Len()),
 		Blocks:   dataBlockIndicesArray,
-		Filename: path[len(path)-1],
+		Filename: GetRelativePathFromAbsolute(filename),
 	}
 
 	// write the inode to the inode table
 	fs.inodes[inodeIndex] = inode
-	err := fs.WriteInodeTable()
+	err = fs.WriteInodeTable()
 	if err != nil {
 		return nil, fmt.Errorf("error writing inode table: %w", err)
 	}
 
 	// write inode contents
-	err = fs.WriteInodeContents(inode.Index, &contents)
+	err = fs.WriteInodeContents(int(inode.Index), contents)
 	if err != nil {
 		return nil, fmt.Errorf("error writing inode contents: %w", err)
 	}
 
-	// update the parent directory
-	err = fs.AddFileToDir(parentInodeIndex, uint32(inodeIndex))
-	if err != nil {
-		return nil, fmt.Errorf("error adding file to directory: %w", err)
-	}
-
 	// update the inode bitmap
 	fs.inodeBitmap[inodeIndex] = 1
+
 	// write the inode bitmap
-	fs.dev.WriteBlock(InodeBitmapIndex, fs.inodeBitmap[:])
+	err = fs.PersistInodeBitmap()
 
 	// update the data bitmap
 	for _, blockIndex := range dataBlockIndices {
 		fs.dataBitmap[blockIndex] = 1
 	}
 	// write the data bitmap
-	fs.dev.WriteBlock(DataBitmapIndex, fs.dataBitmap[:])
+	err = fs.PersistDataBitmap()
+	if err != nil {
+		return nil, fmt.Errorf("error persisting inode bitmap when creating file: %w", err)
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("error persisting data bitmap when creating file: %w", err)
+	}
+
+	// update the parent directory
+	err = fs.AddFileToDir(int(parentInode.Index), inodeIndex)
+	if err != nil {
+		return nil, fmt.Errorf("error adding file to directory: %w", err)
+	}
 
 	return inode, nil
+}
+
+func (fs *FileSystem) FindInodeByName(filename string) (*Inode, error) {
+	path := strings.Split(filename, "/")
+	if path[0] != "" {
+		return nil, fmt.Errorf("filename must be absolute")
+	}
+	return fs.traversePath(path)
+}
+
+func (fs *FileSystem) FindParentInodeByName(filename string) (*Inode, error) {
+	path := strings.Split(filename, "/")
+	if path[0] != "" {
+		return nil, fmt.Errorf("filename must be absolute")
+	}
+	return fs.traversePath(path[:len(path)-1])
+}
+
+func GetRelativePathFromAbsolute(filename string) string {
+	path := strings.Split(filename, "/")
+	if path[0] != "" {
+		return ""
+	}
+	return strings.Join(path[1:], "/")
+}
+
+func (fs *FileSystem) traversePath(path []string) (*Inode, error) {
+	// start at the root inode
+	inodeIndex := 0
+	inode := fs.inodes[inodeIndex]
+	for i := 1; i < len(path); i++ {
+		children, err := fs.ReadDir(inodeIndex)
+		if err != nil {
+			return nil, fmt.Errorf("error reading directory %s: %w", path[i], err)
+		}
+		found := false
+		for _, child := range children {
+			if child.Filename == path[i] {
+				inodeIndex = int(child.Index)
+				inode = child
+				found = true
+				break
+			}
+		}
+		if !found {
+			return nil, fmt.Errorf("directory %s not found", path[i])
+		}
+	}
+
+	return inode, nil
+}
+
+func (fs *FileSystem) FindFreeInode() (int, error) {
+	for i := 0; i < 32; i++ {
+		if fs.inodeBitmap[i] == 0 {
+			return i, nil
+		}
+	}
+
+	return 0, fmt.Errorf("no empty inodes")
+}
+
+func (fs *FileSystem) PersistDataBitmap() error {
+	return fs.dev.WriteBlock(DataBitmapIndex, fs.dataBitmap[:])
+}
+
+func (fs *FileSystem) PersistInodeBitmap() error {
+	return fs.dev.WriteBlock(InodeBitmapIndex, fs.inodeBitmap[:])
+}
+
+func (fs *FileSystem) FindEmptyBlocks(n int) ([]uint32, error) {
+	dataBlockIndices := []uint32{}
+
+	for i := 0; i < 32; i++ {
+		if fs.dataBitmap[i] == 0 {
+			dataBlockIndices = append(dataBlockIndices, uint32(i)+DataStartIndex)
+			if len(dataBlockIndices) == n {
+				break
+			}
+		}
+	}
+
+	if len(dataBlockIndices) != n {
+		return dataBlockIndices, fmt.Errorf("not enough empty data blocks")
+	}
+
+	return dataBlockIndices, nil
+}
+
+// GetSizeInBlocks computes how many blocks n bytes take up
+func GetSizeInBlocks(n int) int {
+	return (n + BlockSize - 1) / BlockSize
 }
 
 type ArrayBlockDevice struct {
@@ -514,4 +638,3 @@ func (dev *ArrayBlockDevice) Dump() {
 	}
 	fmt.Println()
 }
-
--- pkg/fs/fs_test.go
+++ pkg/fs/fs_test.go
@@ -1,6 +1,7 @@
 package fs
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -23,10 +24,10 @@ func TestFSInit(t *testing.T) {
 
 	require.NoError(t, err)
 
-	require.Equal(t, byte(buf[0]), byte(0xb0))
-	require.Equal(t, byte(buf[1]), byte(0xfd))
-	require.Equal(t, byte(buf[2]), byte(0xba))
-	require.Equal(t, byte(buf[3]), byte(0))
+	require.Equal(t, byte(0xb0), byte(buf[0]))
+	require.Equal(t, byte(0xfd), byte(buf[1]))
+	require.Equal(t, byte(0xba), byte(buf[2]))
+	require.Equal(t, byte(0), byte(buf[3]))
 
 	// Test that the initial inode bitmap was properly written
 	buf = make([]byte, BlockSize)
@@ -34,8 +35,12 @@ func TestFSInit(t *testing.T) {
 
 	require.NoError(t, err)
 
-	require.Equal(t, byte(buf[0]), byte(1))
-	require.Equal(t, byte(buf[1]), byte(0))
+	// we start with one inode taken
+	require.Equal(t, byte(1), byte(buf[0]))
+	// all the following inodes are free
+	for i := 1; i < BlockSize; i++ {
+		require.Equal(t, byte(0), byte(buf[i]))
+	}
 
 	// Test that the initial data bitmap was properly written
 	buf = make([]byte, BlockSize)
@@ -43,23 +48,58 @@ func TestFSInit(t *testing.T) {
 
 	require.NoError(t, err)
 
-	require.Equal(t, byte(buf[0]), byte(1))
-	require.Equal(t, byte(buf[1]), byte(0))
+	// All data blocks are free
+	for i := 0; i < BlockSize; i++ {
+		require.Equal(t, byte(0), byte(buf[i]))
+	}
 
 	// Check that the root file was properly written
 	inode, err := filesystem.GetInode(0)
 	require.NoError(t, err)
-	require.Equal(t, *inode, Inode{
-		Size:     0,
-		Index:    0,
-		Type:     InodeTypeDirectory,
-		Filename: "/",
-	})
+	require.Equal(t, uint32(0), inode.Size)
+	require.Equal(t, uint32(0), inode.Index)
+	require.Equal(t, InodeTypeDirectory, inode.Type)
+	require.Equal(t, "/", inode.Filename)
 
 	_, err = filesystem.ReadInodeContents(0)
 	require.NoError(t, err)
 
 	dir, err := filesystem.ReadDir(0)
 	require.NoError(t, err)
-	require.Equal(t, len(dir), 0)
+	require.Equal(t, 0, len(dir))
+}
+
+func TestCreateFile(t *testing.T) {
+	// create a 32KiB array
+	disk := make([]byte, 32*1024)
+	// create a BlockDevice that uses the array as storage
+	dev := NewArrayBlockDevice(disk)
+
+	// create a filesystem on the device
+	filesystem, err := NewFileSystem(dev)
+
+	require.NoError(t, err)
+
+	// Create a file
+	str := "hello world"
+	contents := bytes.NewBuffer([]byte(str))
+	inode, err := filesystem.CreateFile("/foo", contents)
+	require.NoError(t, err)
+
+	// Check that the file was properly written
+	require.Equal(t, inode.Filename, "foo")
+	require.Equal(t, inode.Type, InodeType(InodeTypeFile))
+	require.Equal(t, inode.Size, uint32(len(str)))
+
+	_, err = filesystem.ReadInodeContents(1)
+	require.NoError(t, err)
+
+	// check that the root directory got updated
+	dir, err := filesystem.ReadDir(0)
+	require.NoError(t, err)
+	require.Equal(t, len(dir), 1)
+
+	require.Equal(t, dir[0].Filename, "foo")
+	require.Equal(t, dir[0].Type, InodeType(InodeTypeFile))
+	require.Equal(t, dir[0].Size, uint32(len(str)))
 }
